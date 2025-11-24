3028bc0f4d901cf56d692efc2c5a2b3d26d312aa
Karthigeyan <karthigeyanpers@gmail.com>
2025-11-24 12:22:34 +0530

    Daily linkedlist practice

---
+++ pract.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value){
+    struct Node* newNode=malloc(sizeof(struct Node));
+    newNode->data=value;
+    newNode->next=NULL;
+    return newNode;
+}
+
+struct Node* bn(struct Node* head, int value){
+    struct Node* newNode=createNode(value);
+    newNode->next=head;
+    return newNode;
+}
+
+struct Node* en(struct Node* head, int value){
+    struct Node* newNode=createNode(value);
+    if (head==NULL) return newNode;
+    struct Node* temp=head;
+    while (temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=newNode;
+    return head;
+}
+
+void di(struct Node* head){
+    struct Node* temp=head;
+    while(temp!=NULL){
+        printf("%d->", temp->data);
+        temp=temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main(){
+    struct Node* head=NULL;
+    head=bn(head,10);
+    head=en(head,20);
+    head=en(head,30);
+    di(head);
+}
